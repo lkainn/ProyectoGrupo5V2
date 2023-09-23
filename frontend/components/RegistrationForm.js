90e28789fcebfe1f87691550d93330b1b6ed5093
lkainn <143043638+lkainn@users.noreply.github.com>
2023-09-23 14:39:18 -0300

    arreglado formulario de registro

--- frontend/components/RegistrationForm.js
+++ frontend/components/RegistrationForm.js
@@ -25,7 +25,7 @@ const RegistrationForm = () => {
 
     try {
       // realiza la solicitud POST a la API
-      const response = await fetch('http://127.0.0.1:8000/myauth/register/', {
+      const response = await fetch('http://127.0.0.1:8000/auth/register/', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
